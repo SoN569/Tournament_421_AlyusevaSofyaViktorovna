70e3c627344968c0c02128839859195b1f1cce3c
212102 <alyuseva97@gmail.com>
2024-12-27 09:28:49 +0300

    добавлена бд, начало авторизации

--- Tournament_421_AlyusevaSofyaViktorovna/App.xaml.cs
+++ Tournament_421_AlyusevaSofyaViktorovna/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Tournament_421_AlyusevaSofyaViktorovna.Components;
 
 namespace Tournament_421_AlyusevaSofyaViktorovna
 {
@@ -13,5 +14,6 @@ namespace Tournament_421_AlyusevaSofyaViktorovna
     /// </summary>
     public partial class App : Application
     {
+        public static TournamentDB_421_AlyusevaSofyaEntities db = new TournamentDB_421_AlyusevaSofyaEntities();
     }
 }
